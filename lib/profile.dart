34dff0a969ad909947f551dd06a1777edc77e4b9
Dhiyasiraj <dhiyasiraj419@gmail.com>
2025-09-02 13:02:31 +0530

    prfle

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Profile extends StatelessWidget {
-const Profile({super.key});
+  const Profile({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -11,10 +11,7 @@ const Profile({super.key});
           gradient: RadialGradient(
             center: Alignment.topLeft, // Start from top-left
             radius: 0.7,
-            colors: [
-              Color(0xFF0C4556),
-              Colors.white,
-            ],
+            colors: [Color(0xFF0C4556), Colors.white],
             stops: [0.0, 1.0],
           ),
         ),
@@ -26,77 +23,187 @@ const Profile({super.key});
                 gradient: RadialGradient(
                   center: Alignment.bottomRight,
                   radius: 0.7,
-                  colors: [
-                    Color(0xFF0C4556),
-                    Colors.transparent,
-                  ],
+                  colors: [Color(0xFF0C4556), Colors.transparent],
                   stops: [0.0, 1.0],
                 ),
               ),
             ),
-          Padding(padding: EdgeInsets.only(left: 30, right: 30),),
-           Column(
+            Padding(padding: EdgeInsets.only(left: 30, right: 30)),
+            Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(height: 110),
+                Container(
+                  height: 300,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF0C4556),
+                    borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(30),
+                      bottomLeft: Radius.circular(30),
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.arrow_back_ios,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          Text(
+                            'Profile',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        'Set up your profile',
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        'Update your profile to connect your doctor with better impression.',
+                        style: TextStyle(color: Colors.white, fontSize: 14),
+                      ),
+                      SizedBox(height: 20),
+                      CircleAvatar(
+                        radius: 75,
+                        backgroundColor: Colors.grey,
+                        child: Icon(Icons.camera_alt_outlined, size: 40),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 30),
+                Text(
+                  'Personal information',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 20),
                 Padding(
-                  padding: EdgeInsets.only(left: 10),
-                  child: Text("Profile", style: TextStyle(fontSize: 24)),
-                ),
-                SizedBox(height: 70),
-                Padding(
-                  padding: const EdgeInsets.only(left: 8),
-                  child: Text(
-                    "personal Information",
-                    style: TextStyle(fontSize: 19),
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 65,
+                        width: double.infinity,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Name',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              Text('John Doe'),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 65,
+                        width: double.infinity,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Contact Number',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              Text('+123 456 7890'),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 65,
+                        width: double.infinity,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Date of Birth',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              Text('DD/MM/YYYY'),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 65,
+                        width: double.infinity,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Location',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              Text('Add details'),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 30),
+             SizedBox(height: 55,width: 250,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromRGBO(12, 69, 86, 72),
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-                SizedBox(height: 50),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Name',
-                    hintText:" Enter your full name" ,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                  ),
+                child: Text(
+                  "Continue",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
-                SizedBox(height: 16),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Contact number',
-                    hintText: "xxx-xxxx-xxx",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 16),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Date of Birth',
-                    hintText: "DD/MM/YYYY",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 16),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Location',
-                    hintText: "Add detailes",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
+              ),
+            ),
+                    ],
                   ),
                 ),
               ],
             ),
-                
-        ],
+          ],
+        ),
       ),
-    ),
     );
   }
 }
